8531631adf1553522b0ae26457f6690aeaf77f5a
chuchangman <chuchangman@gmail.com>
2026-04-17 09:59:11 +0900

    feat: User API - 내 정보 조회/수정 구현

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import GitHubLoginCallbackView, LogoutView, login
+from .views import GitHubLoginCallbackView, LogoutView, login, UserMeView
 
 urlpatterns = [
     path('callback/', GitHubLoginCallbackView.as_view()),
     path('login/', login),
     path('logout/', LogoutView.as_view()),
+    path('me/', UserMeView.as_view()),
 ]
