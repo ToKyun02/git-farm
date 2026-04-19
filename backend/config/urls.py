008e8b36e6c13790b4583780ae06a0a5d72c3484
추창만 <chuchangman@gmail.com>
2026-04-19 19:07:04 +0900

    refactor : 불필요한 코드 삭제

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,18 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenVerifyView,
-)
+
 from accounts.views import CookieTokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
     # JWT 인증
-    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
-    path('api/auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
     # Social OAuth (GitHub, Google 등)
     path('api/auth/social/', include('social_django.urls', namespace='social')),
